aa657a402bc061f02b334d155cbe937bb24a3f06
Kyle Kotowich <kkotowich39@gmail.com>
2017-10-28 15:01:14 -0500

    Add routing

--- client/main.jsx
+++ client/main.jsx
@@ -2,11 +2,25 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import { BrowserRouter, HashRouter, Switch, Route } from 'react-router-dom';
 
-import App from '../imports/ui/App.jsx';
+import App from '../imports/ui/App';
+import Example from '../imports/ui/Example';
+import Lost from '../imports/ui/Lost';
 
 injectTapEventPlugin();
 
 Meteor.startup( () => {
-  render(<App />, document.getElementById('render-target'));
+  render(
+    (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={App} />
+          <Route exact path="/example" component ={Example} />
+          <Route exact path="*" component={Lost} />
+        </Switch>
+      </BrowserRouter>
+    )
+    , document.getElementById('render-target')
+  );
 });
