ad0c379c79209ceb3260fadc890304e6b0df4f12
Kyle Kotowich <kkotowich39@gmail.com>
2017-10-29 11:16:28 -0500

    Add user login with accounts-google

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -4,7 +4,7 @@ import RaisedButton from 'material-ui/RaisedButton';
 import AppBar from 'material-ui/AppBar';
 import { List } from 'material-ui/List';
 import Divider from 'material-ui/Divider';
-import { createContainer } from 'react-meteor-data';
+import { createContainer } from 'meteor/react-meteor-data';
 import { Link } from 'react-router';
 
 // database - collection
@@ -13,6 +13,7 @@ import { Players } from '../api/players';
 import TeamList from './Team-list';
 import TeamStats from './Team-stats';
 import Player from './Player';
+import AccountsWrapper from './AccountsWrapper';
 
 export default class App extends Component {
   constructor(props) {
@@ -35,11 +36,13 @@ export default class App extends Component {
           <AppBar
             title="Soccer Application"
             iconClassNameRight="muidocs-icon-navigation-expand-more"
-            showMenuIconButton={false}/>
+            showMenuIconButton={false}>
+              <AccountsWrapper />
+            </AppBar>
           <div className="row">
             <div className="col s12 m7" ><Player /></div>
             <div className="col s12 m5" >
-              <h2>Team list</h2> <Link to="/new" className="waves-effect waves-light btn">Add player</Link>
+              <h2>Team list</h2><Link to="/new" className="waves-effect waves-light btn">Add player</Link>
               <Divider/>
                 <List>
                   {this.renderPlayers()}
@@ -55,13 +58,14 @@ export default class App extends Component {
 }
 
 App.propTypes = {
-  player: PropTypes.array.isRequired
-}
+  players: PropTypes.array.isRequired,
+};
 
-export default createContainer( () => {
-  Meteor.subscribe("players");
+export default createContainer(() => {
+  Meteor.subscribe('players');
+  const user = Meteor.userId();
 
   return {
-    players: Players.find({}, {sort: {name: 1}}).fetch()
+    players: Players.find({ owner: user }, {sort: { name: 1}}).fetch(),
   };
 }, App);
