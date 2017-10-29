eabacc47c7391dbf4bb4950cd75708133366877d
Kyle Kotowich <kkotowich39@gmail.com>
2017-10-28 20:58:47 -0500

    Add new player feature

--- client/main.jsx
+++ client/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
-//import injectTapEventPlugin from 'react-tap-event-plugin';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import { Router, Route, browserHistory } from 'react-router';
 
 import App from '../imports/ui/App.jsx';
 import New from '../imports/ui/New';
 import Lost from '../imports/ui/Lost';
 
-//injectTapEventPlugin();
+injectTapEventPlugin();
 
 Meteor.startup(() => {
   render((
